cc6b9e234dbb16e1f797f1b22daa903cf269c92f
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2022-12-21 13:20:02 +0200

    bug

--- generate_signal.m
+++ generate_signal.m
@@ -2,28 +2,28 @@ function des_signal = generate_signal(signal_num,time)
 switch signal_num
    case 1
         
-        amp = input('please enter the Amplitude :');
+        amp1 = input('please enter the Amplitude :');
         
         freq = input('please enter the Frequency : ');
       
         phase = input('please enter the Phase :');
         
-        des_signal = amp*sin(2*pi*time*freq + phase);
+        des_signal = amp1*sin(2*pi*time*freq + phase);
     
     case 2
         
-        amp = input('please enter the Amplitude :');
+        amp2 = input('please enter the Amplitude :');
        
         len = length(time);
         
-        des_signal = amp*ones(1,len);
+        des_signal = amp2*ones(1,len);
     case 3
         
-        amp = input('please enter the Amplitude :');
+        amp3 = input('please enter the Amplitude :');
        
         intercept = input('please enter the intercept :');
         
-        des_signal = amp*time+intercept;
+        des_signal = (amp3*time)+intercept;
     
    
     case 4
@@ -34,7 +34,7 @@ switch signal_num
         des_signal = 0;
         while (i<=power+1)
             coeffenets(i)=input('please enter the coeffenets of the signal from higher to lower order:');
-            des_signal=portion_signal+coeffenets(i)*time.^((power+1)-i);
+            des_signal=des_signal+coeffenets(i)*time.^((power+1)-i);
             i=i+1;
         end
         
